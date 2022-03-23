6ad8515e4ed5d6e8ce14790b1deea937953bb658
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-23 16:47:18 +0000

    added code

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string streetName;
+        int streetNumber;
+
+        Console.WriteLine("This program gets a user's name and age.");
+        Console.WriteLine("");
+
+      Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine()); 
+      Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + streetNumber + " " + streetName + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
